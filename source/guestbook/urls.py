9be1b9efb9c771421addcdb6a3e47782f68e9d32
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-25 15:02:59 +0600

    в guestbook/urls.py добавлена ссылка на создание отзыва

--- source/guestbook/urls.py
+++ source/guestbook/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from guestbook.views.view_guestbook import index_view
+from guestbook.views.view_guestbook import create_review, index_view
 
 urlpatterns = [
-    path('', index_view),
+    path('', index_view, name='home'),
+    path('add/review', create_review, name='create_review')
 ]
