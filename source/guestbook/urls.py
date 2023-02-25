a2c650b8edf92a07b9265af75d4d70f2be7bdc39
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-25 17:45:12 +0600

    в guestbook/urls.py добавлена ссылка на удаление

--- source/guestbook/urls.py
+++ source/guestbook/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from guestbook.views.view_guestbook import create_review, index_view, update_review
+from guestbook.views.view_guestbook import create_review, delete_review, index_view, update_review
 
 urlpatterns = [
     path('', index_view, name='home'),
     path('add/review/', create_review, name='create_review'),
     path('update/review/<int:pk>', update_review, name='update_review'),
+    path('delete/review/<int:pk>', delete_review, name='delete_review'),
+    path('confirm/delete/review/<int:pk>', delete_review, name='confirm_delete_review'),
+
 ]
