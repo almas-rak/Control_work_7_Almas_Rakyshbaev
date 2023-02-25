a78e7b7b5c1ee7cf9968f97be5fe7a16b6eada28
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-25 12:45:15 +0600

    создание модели гостевой книги и регистрация в админке

--- source/guestbook/admin.py
+++ source/guestbook/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from guestbook.models import GuestReview
+
+
 # Register your models here.
+
+class GuestReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'email', 'description', 'created_at', 'updated_at', 'status')
+
+
+admin.site.register(GuestReview, GuestReviewAdmin)
--- source/guestbook/models.py
+++ source/guestbook/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class GuestReview(models.Model):
+    STATUS_CHOICES = (
+        ('active', 'Активно'),
+        ('blocked', 'Заблокировано')
+    )
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    description = models.TextField(max_length=3000, verbose_name='Запись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата и время редактирования')
+    status = models.CharField(max_length=13, choices=STATUS_CHOICES, default='active')
+
+    def __str__(self):
+        return f'{self.author} ({self.email})'
