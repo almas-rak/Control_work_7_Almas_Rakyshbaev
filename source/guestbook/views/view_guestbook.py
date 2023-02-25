8f785cc1f865961e5bd42b18960989624cb35e7b
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-25 14:59:31 +0600

    в view_guestbook.py добавлен метод создания

--- source/guestbook/views/view_guestbook.py
+++ source/guestbook/views/view_guestbook.py
@@ -1,9 +1,23 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
+from guestbook.forms import GuestReviewForm
 from guestbook.models import GuestReview
 
 
 def index_view(request: WSGIRequest):
-    reviews = GuestReview.objects.filter(status = 'active')
-    return render(request, 'index.html', context={'reviews': reviews} )
+    reviews = GuestReview.objects.filter(status='active')
+    return render(request, 'index.html', context={'reviews': reviews})
+
+
+def create_review(request: WSGIRequest):
+    if request.method == 'GET':
+        form = GuestReviewForm()
+        return render(request, 'create_review.html', context={'form': form})
+    else:
+        form = GuestReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            return render(request, 'create_review.html', context={'form': form})
